ed6fa1055959ca12d54b5c8a5b5ab2732d14dc0e
sarthak <sarthak75220@gmail.com>
2024-11-04 17:53:01 +0530

    react-start

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,23 @@
+ const App=() => {
+  return (
+    <>
+    <h2>Name: Game of Thrones</h2>
+    <h3>Rating: 9.2</h3>
+    <p>
+      Summary:*Game of Thrones* is a fantasy 
+      series where noble families vie for the 
+      Iron Throne in the Seven Kingdoms of Westeros. 
+      Amid political intrigue, betrayals, and alliances,
+      an ancient threat—the White Walkers—emerges. With dragons,
+      magic, and complex characters, the series explores power,
+      loyalty,and the harsh costs of ambition.
+    </p>
+    <div>
+      <img src="got.jpg" alt=""  width="40%" height="40%" />
+    </div>
+    </>
+  );
+}
+
+
+export default App ;
