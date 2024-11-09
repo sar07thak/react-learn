189ab813414df52238875b2fa909fa47d62b9386
sarthak <sarthak75220@gmail.com>
2024-11-09 19:30:14 +0530

    update

--- react-card/src/components/seriesCard.jsx
+++ react-card/src/components/seriesCard.jsx
@@ -1,11 +1,11 @@
 export const Seriescard = ( {curElem } ) => {
     return(
-        <li id="key" className='min-w-80 font-mono flex flex-col justify-between w-[50%] items-center rounded-md shadow-md shadow-blue-600'>
-        <div className="p-1 border">
-            <img src={curElem.img_url} alt="Game of Thrones" className="h-32 w-72   object-cover " />
+        <li id="key" className='min-w-[250px] h-[30em]  w-[100%] font-mono flex flex-col justify-between items-center rounded-md shadow-md shadow-blue-600'>
+        <div className="mb-16 p-1 h-40  w-[70%]  ">
+            <img src={curElem.img_url} alt="Game of Thrones" className=" w-[100%]  object-cover " />
         </div>
-        <h1 className="text-lg  w-40 text-center border font-bold">Name : {curElem.name}</h1>
-        <div className=" w-[60%] border p-2">
+        <h1 className=" text-lg   text-center font-bold">Name : {curElem.name}</h1>
+        <div className="  p-2">
         <p>Rating : <span className={curElem.rating <=8.5 ? ' bg-orange-400  w-32 p-1.5 rounded-xl text-white ' : 'text-white c bg-green-400  w-32 p-1.5 rounded-xl ' }> ⭐{curElem.rating}  </span> </p>
         <p>Summary : {curElem.description} </p>
         <p>Genre : {curElem.genre}</p>
--- react-card/src/series.jsx
+++ react-card/src/series.jsx
@@ -5,8 +5,8 @@ import { Seriescard } from './components/seriesCard' ;
 export const MovieSeries = () => {
     return(
         <>
-        <h1 className='border-b text-blue-600 border-blue-600 p-4 font-mono flex text-2xl justify-center'>List of best series :</h1>
-        <ul className='p-4 grid grid-rows-3 grid-cols-3 gap-4 '>
+        <h1 className='border-b text-blue-600 border-blue-600 p-4 font-mono flex flex-wrap text-2xl justify-center'>List of best series :</h1>
+        <div className='p-4 grid md:grid-rows-3 md:grid-cols-3 sm:grid-rows-2 sm:grid-cols-2 gap-4 '>
             {
                 Data.map((curElem)=>{
                 return (
@@ -14,7 +14,7 @@ export const MovieSeries = () => {
                 ) 
                 })
             }
-        </ul>     
+        </div>     
         </>
     )
 }
