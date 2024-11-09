fba32bf762c578ed45c1671360a1baf28e626cad
sarthak <sarthak75220@gmail.com>
2024-11-09 11:27:05 +0530

    update

--- react-card/src/components/seriesCard.jsx
+++ react-card/src/components/seriesCard.jsx
@@ -1,18 +1,20 @@
 export const Seriescard = ( {curElem } ) => {
     return(
-        <li id="key" className=' font-mono flex justify-between mb-2 items-center rounded-md shadow-md shadow-blue-600'>
-        <h1 className="text-lg  w-40 text-center  font-bold">Name : {curElem.name}</h1>
-        <div className="p-1">
-            <img src={curElem.img_url} alt="Game of Thrones" className="h-32 w-32 rounded-full  object-cover " />
+        <li id="key" className='min-w-80 font-mono flex flex-col justify-between w-[50%] items-center rounded-md shadow-md shadow-blue-600'>
+        <div className="p-1 border">
+            <img src={curElem.img_url} alt="Game of Thrones" className="h-32 w-72   object-cover " />
         </div>
-        <div className=" w-[60%]  p-2">
-        <p>Rating : {curElem.rating}</p>
+        <h1 className="text-lg  w-40 text-center border font-bold">Name : {curElem.name}</h1>
+        <div className=" w-[60%] border p-2">
+        <p>Rating : <span className={curElem.rating <=8.5 ? ' bg-orange-400  w-32 p-1.5 rounded-xl text-white ' : 'text-white c bg-green-400  w-32 p-1.5 rounded-xl ' }> ⭐{curElem.rating}  </span> </p>
         <p>Summary : {curElem.description} </p>
         <p>Genre : {curElem.genre}</p>
         <p>Cast : {curElem.cast}</p>
-        <a href={curElem.watch_url}><button className=" w-36 p-1 bg-blue-600 text-white rounded-lg text-lg mt-1">Watch-Now</button></a> 
+        <a href={curElem.watch_url}><button className={curElem.rating <=8.5 ? 'w-36 p-1 bg-orange-400 cursor-pointer hover:bg-orange-600 text-white rounded-lg text-lg mt-1' : 'w-36 p-1 cursor-pointer hover:bg-green-600 bg-green-400 font-medium text-white rounded-lg text-lg mt-1'}>Watch-Now</button></a> 
         </div>
         
         </li>
       )
 }
+
+// w-36 p-1 bg-blue-600 text-white rounded-lg text-lg mt-1
--- react-card/src/series.jsx
+++ react-card/src/series.jsx
@@ -5,8 +5,8 @@ import { Seriescard } from './components/seriesCard' ;
 export const MovieSeries = () => {
     return(
         <>
-        <h1 className='border-b border-blue-600 p-4 font-mono flex text-2xl justify-center'>List of best series :</h1>
-        <ul className='p-4 grid grid-rows-2 grid-cols-2 gap-4'>
+        <h1 className='border-b text-blue-600 border-blue-600 p-4 font-mono flex text-2xl justify-center'>List of best series :</h1>
+        <ul className='p-4 grid grid-rows-3 grid-cols-3 gap-4 '>
             {
                 Data.map((curElem)=>{
                 return (
