346be412b943021f31a36edc997e45851d9d4268
sarthak <sarthak75220@gmail.com>
2024-11-13 21:11:14 +0530

    color-changer

--- background-changer/src/App.jsx
+++ background-changer/src/App.jsx
@@ -2,36 +2,40 @@ import { useState } from "react";
 
 export const App = () => {
 
-  let [color,setColor] = useState("coral");
+  let [color,setColor] = useState("#1E293B");
 
   
   return(
     <>
-      <div className="h-screen flex justify-center items-center text-white duration-300" style={{backgroundColor:color}}>
+      <div className="h-screen flex  flex-col justify-center items-center text-white duration-300" style={{backgroundColor:color}}>
+        <h1 className="mb-2 text-2xl ">Background-Changer</h1>
         <div className="bg-white flex w-[80%]  justify-evenly p-4 rounded-md flex-wrap gap-0.5">
-          <button className="p-1 w-36 border bg-red-500 hover:border-red-800 duration-200 rounded-md" onClick={() => {
+          <button className="relative z-0 rounded bg-red-500 px-10 py-3 transition-[all_0.3s_ease] after:absolute after:left-0 after:top-0 after:-z-10 after:h-full after:w-0 after:rounded after:bg-red-700 after:transition-[all_0.3s_ease]  hover:after:w-full" onClick={() => {
             setColor("#EF4444");
           }}>Red</button>
-          <button className="p-1 w-36 border bg-blue-500 duration-200 hover:border-blue-800 rounded-md" onClick={()=>{
+          <button className="relative z-0 rounded bg-blue-500 px-10 py-3 transition-[all_0.3s_ease] after:absolute after:left-0 after:top-0 after:-z-10 after:h-full after:w-0 after:rounded after:bg-blue-700 after:transition-[all_0.3s_ease]  hover:after:w-full" onClick={()=>{
             setColor("#3B82F6");
           }}>Blue</button>
-          <button className="p-1 w-36 border bg-yellow-400 duration-200 hover:border-yellow-600 rounded-md" onClick={()=>{
+          <button className="relative z-0 rounded bg-yellow-500 px-10 py-3 transition-[all_0.3s_ease] after:absolute after:left-0 after:top-0 after:-z-10 after:h-full after:w-0 after:rounded after:bg-yellow-700 after:transition-[all_0.3s_ease]  hover:after:w-full" onClick={()=>{
             setColor("#FACC15");
           }}>Yellow</button>
-          <button className="p-1 w-36 border bg-green-500 duration-200 hover:border-green-800 rounded-md" onClick={()=>{
+          <button className="relative z-0 rounded bg-green-500 px-10 py-3 transition-[all_0.3s_ease] after:absolute after:left-0 after:top-0 after:-z-10 after:h-full after:w-0 after:rounded after:bg-green-700 after:transition-[all_0.3s_ease]  hover:after:w-full" onClick={()=>{
             setColor("#22C55E");
           }}>Green</button>
-          <button className="p-1 w-36 border bg-slate-500  duration-200 hover:border-slate-800 rounded-md" onClick={()=>{
+          <button className="relative z-0 rounded bg-slate-500 px-10 py-3 transition-[all_0.3s_ease] after:absolute after:left-0 after:top-0 after:-z-10 after:h-full after:w-0 after:rounded after:bg-slate-700 after:transition-[all_0.3s_ease]  hover:after:w-full" onClick={()=>{
             setColor("#64748B");
           }}>Grey</button>
-          <button className="p-1 w-36 border bg-white text-black  duration-200 hover:border-slate-900 rounded-md" onClick={()=>{
+          <button className="relative z-0 rounded text-black hover:text-white bg-white px-10 py-3 transition-[all_0.3s_ease] after:absolute after:left-0 after:top-0 after:-z-10 after:h-full after:w-0 after:rounded after:bg-slate-800 after:transition-[all_0.3s_ease]  hover:after:w-full" onClick={()=>{
             setColor("white");
           }}>White</button>
-          <button className="p-1 w-36 border bg-purple-500  duration-200 hover:border-purple-800 rounded-md" onClick={()=>{
+          <button className="relative z-0 rounded bg-purple-500 px-10 py-3 transition-[all_0.3s_ease] after:absolute after:left-0 after:top-0 after:-z-10 after:h-full after:w-0 after:rounded after:bg-purple-700 after:transition-[all_0.3s_ease]  hover:after:w-full" onClick={()=>{
             setColor("#8B5CF6");
           }}>Purple</button>
+
         </div>
       </div>
     </>
   );
 }
+
+// 
