b60057cd15908a7befafa8825e594d190051acd4
sarthak <sarthak75220@gmail.com>
2024-11-17 16:09:34 +0530

    date and time

--- to-do-list/src/Todo.jsx
+++ to-do-list/src/Todo.jsx
@@ -1,37 +1,54 @@
 // import images from "../public/images";
 import { CiCircleCheck } from "react-icons/ci";
 import { TiDeleteOutline } from "react-icons/ti";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export const ToDoList = () => {
 
   let [userInput , setInput ] = useState("");
   let [task, setTask] = useState([]);
+  let [ DateTime , setDateTime ] = useState("");
+
 
   const handleEventChange = (value) => {
     setInput(value);
+    
   }
 
   const handleformSubmit = (event) =>{
     event.preventDefault() ;
     
-      if ( !userInput) return alert("Enter some tasks") ;
+    if ( !userInput) return alert("Enter some tasks") ;
       
-      setTask( (prevTask) => [...prevTask , userInput ])
-
-      if (task.includes(userInput)) return ;
+    setTask( (prevTask) => [...prevTask , userInput.trim() ])
       
-      setInput("");
+    setInput("");
   }
+
+useEffect(()=>{
+  const interval = setInterval(() => {
+    const date = new Date() ;
+    const formattedDate = date.toLocaleDateString();
+  
+    const formattedTime = date.toLocaleTimeString();
+    setDateTime(`${formattedDate}-${formattedTime}`)
+    
+    
+  }, 1000);
+
+  return () => clearInterval(interval);
+})
 
 
   
+
   return(
     <>
      <div class="container min-h-screen w-screen bg-indigo-300 flex flex-col p-10 items-center  font-mono ">
       <div>
           <header className="text-center mb-5">
             <h1 className="text-6xl font-bold text-white"> To-Do-List</h1>
+            <h2 className="text-2xl text-white font-bold"> {DateTime}  </h2>
           </header>
           <section>
             <form className="flex " onSubmit={handleformSubmit}>
@@ -59,16 +76,18 @@ export const ToDoList = () => {
 
               <ul>
                 {
-                  task.map((curTask, index) => {
+                  task.map((curTask , index) => {
                     return (
                       <>
                       <li key ={index} className="p-2.5 rounded-md mb-2 bg-white flex justify-between items-center " > 
                          <span>{curTask}</span>
+
                          <div className="flex justify-between  w-20">
                          <button className="text-white relative z-0  rounded-md bg-teal-400
                          p-2  transition-[all_0.3s_ease] after:absolute after:left-0
                          after:top-0 after:-z-10 after:h-full after:w-0 after:rounded after:bg-teal-700 
-                         after:transition-[all_0.3s_ease]  hover:after:w-full"> <CiCircleCheck /> </button>
+                         after:transition-[all_0.3s_ease]  hover:after:w-full" > <CiCircleCheck /> </button>
+
                          <button className="text-white relative z-0 rounded-md bg-red-400
                          p-2.5  transition-[all_0.3s_ease] after:absolute after:left-0
                          after:top-0 after:-z-10 after:h-full after:w-0 after:rounded after:bg-red-600 
