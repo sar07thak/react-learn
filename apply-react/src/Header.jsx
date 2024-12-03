7c903bbee0994c1f1d7e8ab44e4fdac827894ca4
sarthak <sarthak75220@gmail.com>
2024-12-03 10:48:25 +0530

    updt

--- apply-react/src/Header.jsx
+++ apply-react/src/Header.jsx
@@ -1,23 +1,26 @@
 export const Header =() => {
     return(
         <>
-        <div className="min-h-20 bg-slate-500 border text-white flex flex-row ">
-            <h2>
-                <Div/>
-                <Div/>
-                
-            </h2>
+        <div className="min-h-20 bg-slate-600  text-white flex flex-row items-center justify-between p-2 ">
+        <Div name ="E-commerce" />
+            <div className="flex  w-[60%] justify-between">
+                <Div name = "home" />
+                <Div name= "our-projects" />
+                <Div name = "contact-us"/>
+                <Div name = "testimonial"/>
+                <Div name = "apply-change"/>
+            </div>
         </div>
         </>
     );
 }
 
 
-const Div = () => {
+const Div = ({name}) => {
     return(
         <>
-        <div className="bg-slate-800 h-10 w-15 text-white p-2 ">
-            <span>head</span>
+        <div className="bg-slate-800 h-10 w-40 text-white p-2 text-center rounded-lg ">
+            <span>{name}</span>
         </div>
         </>
     );
--- apply-react/src/main.jsx
+++ apply-react/src/main.jsx
@@ -2,9 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import { Header } from './Header'
+import { Content } from './Content'
+import { Footer } from './Footer'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Header/>
+    <Content/>
+    <Footer/>
   </StrictMode>,
 )
